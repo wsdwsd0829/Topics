82bf46d063295ece84e2132cb5d126a5260e135b
Max Wang <max.wang.cs@gmail.com>
2019-11-01 20:46:34 -0700

    hole in ch1

--- examples/UICollectionViewBookCode/Chapter 1/Chapter 1 Project 1/Chapter 1 Project 1/AFViewController.m
+++ examples/UICollectionViewBookCode/Chapter 1/Chapter 1 Project 1/Chapter 1 Project 1/AFViewController.m
@@ -19,17 +19,13 @@ static NSString *kCellIdentifier = @"Cell Identifier";
     
     [self.collectionView registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:kCellIdentifier];
     
-    const NSInteger numberOfColors = 100;
+    const NSInteger numberOfColors = 0; // TODO change to 100;
     
     NSMutableArray *tempArray = [NSMutableArray arrayWithCapacity:numberOfColors];
     
     for (NSInteger i = 0; i < numberOfColors; i++)
     {
-        CGFloat redValue = (arc4random() % 255) / 255.0f;
-        CGFloat blueValue = (arc4random() % 255) / 255.0f;
-        CGFloat greenValue = (arc4random() % 255) / 255.0f;
-        
-        [tempArray addObject:[UIColor colorWithRed:redValue green:greenValue blue:blueValue alpha:1.0f]];
+        // TODO: create RGB value using arc4random
     }
     
     colorArray = [NSArray arrayWithArray:tempArray];
