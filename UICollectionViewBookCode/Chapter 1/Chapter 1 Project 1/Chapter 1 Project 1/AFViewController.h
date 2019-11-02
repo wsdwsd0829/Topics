9d447ea873333a9597145184adaed08f68e04b37
Max Wang <max.wang.cs@gmail.com>
2019-11-01 20:17:55 -0700

    add collection view book code

---
+++ UICollectionViewBookCode/Chapter 1/Chapter 1 Project 1/Chapter 1 Project 1/AFViewController.h
@@ -0,0 +1,11 @@
+//
+//  AFViewController.h
+//  Chapter 1 Project 1
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AFViewController : UICollectionViewController
+
+@end
---
+++ UICollectionViewBookCode/Chapter 4/Cover Flow/Cover Flow/AFCollectionViewLayoutAttributes.m
@@ -0,0 +1,27 @@
+//
+//  AFCollectionViewLayoutAttributes.m
+//  Cover Flow
+//
+//  Created by Ash Furrow on 2013-01-17.
+//  Copyright (c) 2013 Ash Furrow. All rights reserved.
+//
+
+#import "AFCollectionViewLayoutAttributes.h"
+
+@implementation AFCollectionViewLayoutAttributes
+
+-(id)copyWithZone:(NSZone *)zone
+{
+    AFCollectionViewLayoutAttributes *attributes = [super copyWithZone:zone];
+    
+    attributes.shouldRasterize = self.shouldRasterize;
+    attributes.maskingValue = self.maskingValue;
+    
+    return attributes;
+}
+
+-(BOOL)isEqual:(AFCollectionViewLayoutAttributes *)object {
+    return [super isEqual:object] && (self.shouldRasterize == object.shouldRasterize && self.maskingValue == object.maskingValue);
+}
+
+@end
